e8d06cfbfe6f37993200b1d5d193ae61653145ac
Paveloom <paveloom@mail.ru>
2019-09-11 19:00:37 +0300

    > Больше функций:
    Добавлена родовая функция f для вычисления значений как от скаляра (f_scalar), так и от массива (f_array). Описан прототип функции gauss_quad_unit для вычисления интеграла на отрезке [-1, 1].

--- H1/Исходники/integrand.f95
+++ H1/Исходники/integrand.f95
@@ -1,16 +1,36 @@
 module integrand
 implicit none
      
+     interface f
+     module procedure f_scalar, f_array
+     end interface f
+
      contains
      
-     function f(x)
+     function f_scalar(x)
      implicit none
           
           real(8), intent(in) :: x
-          real(8) :: f
+          real(8) :: f_scalar
 
-          f = (x + 1d0) * dsin(x)
+          f_scalar = (x + 1d0) * dsin(x)
 
-     end function f
+     end function f_scalar
+
+     function f_array(x)
+     implicit none
+          
+          real(8), intent(in), dimension(:) :: x
+          real(8), dimension(size(x)) :: f_array
+
+          integer(4) :: i ! Вспомогательная переменная
+
+          do i = 1, size(x)
+
+               f_array(i) = f_scalar(x(i))
+
+          enddo
+          
+     end function f_array
      
 end module integrand
--- H1/Исходники/subprograms.f95
+++ H1/Исходники/subprograms.f95
@@ -4,12 +4,40 @@ implicit none
      
      contains
 
-     subroutine test
+     subroutine gauss_quad_init(n, a, b, t, w)
      implicit none
           
-          write(*,*) 'oof'
+          integer(4), intent(in) :: n ! Число узлов (степень полинома Лежандра)
+          real(8), intent(in) :: a, b ! Границы промежутка
+
+          ! Корни многочлена Лежандра степени n и соответствующие им веса
+          real(8), intent(in), dimension(n) :: t, w
+
+          if ( a - b .lt. 1e-5 ) then
+
+               stop 'Границы промежутка либо совпадают, либо слишком близки.'
+
+          else
+
+               if ( ( ( a - 1d0 ) .lt. 1e-5 ) .and. ( ( b + 1d0 ) .lt. 1e-5 ) ) write(*,*) gauss_quad_unit(n, t, w)
+
+          endif
+
+     end subroutine gauss_quad_init
+
+     function gauss_quad_unit(n, t, w) result(int)
+     implicit none
           
-     end subroutine test
+          integer(4), intent(in) :: n ! Число узлов (степень полинома Лежандра)
+
+          ! Корни многочлена Лежандра степени n и соответствующие им веса
+          real(8), intent(in), dimension(n) :: t, w
+
+          real(8) :: int ! Результат интегрирования
+
+          int = sum( f(t) * w )
+
+     end function gauss_quad_unit
      
 
 end module subprograms
